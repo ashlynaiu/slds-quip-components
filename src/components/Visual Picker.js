428d083c221839c51863f5bf37e1a4cf61df1d8a
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2019-07-18 13:00:23 -0700

    broken state checkpoint

--- src/components/Visual Picker.js
+++ src/components/Visual Picker.js
@@ -2,28 +2,38 @@ import React, { Component } from 'react';
 import UtilityIcons from '@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg';
 
 class VisualPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { isChecked: false }
+  }
 
+  handleChange() {
+    this.setState({isChecked: !this.state.isChecked });
+  }
   render() {
-
+    console.log(this.state.isChecked);
     return (
         <div className="visual-picker">
           <fieldset class="">
             <legend className="label">Select an option</legend>
-              <div className="visual-picker-container">
-                <input type="radio" id="v" value="" name="options" />
-                <label for="">
-                  <span className="input-item">
-                    <span>
-                      <span className="visual-header">Item Text</span>
-                      <span className="visual-description">Some optional item description to help the user better understand what this option is about.</span>
+              <div className="form-control">
+                <div className="visual-picker-container">
+                  <input type="radio" id="visual-picker-1" value="visual-picker-1" name="options" onChange={this.handleChange}/>
+                  <label htmlFor="visual-picker-1">
+                    <span className="input-item">
+                      <span>
+                        <span className="visual-header">Item Text</span>
+                        <span className="visual-description">Some optional item description to help the user better understand what this option is about.</span>
+                      </span>
                     </span>
-                  </span>
-                  <span className="">
-                    <svg className="" aria-hidden="true">
-                      <use xlinkHref={`${UtilityIcons}#/check`}></use>
-                    </svg>
-                  </span>
-                </label>
+                    <span className="visual-check">
+                      <svg className="" aria-hidden="true">
+                        <use xlinkHref={`${UtilityIcons}#/check`}></use>
+                      </svg>
+                    </span>
+                  </label>
+                </div>
               </div>
           </fieldset>
         </div>
