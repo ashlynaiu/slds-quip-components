1db724f92bca6250a134579941ba0ee219a46f14
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2019-07-19 13:28:47 -0700

    little clean

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # slds-quip-components
+Playground for building Quip components using SLDS as a base. 
+
+## Run Locally
+```
+npm install
+npm start
+http://localhost:3000/
+```
---
+++ src/components/Not Found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <h2>Oops, nothing here to see.</h2>
+    )
+  }
+}
+
+export default NotFound;
