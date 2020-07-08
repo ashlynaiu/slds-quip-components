59733a676a084f909be123a18d8c5d544d9d73cc
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2020-07-08 09:25:48 -0700

    finish card management design

--- src/components/Animation Components/Animation.js
+++ src/components/Animation Components/Animation.js
@@ -3,12 +3,28 @@ import '../../styles/animation.sass';
 import Card from './Card';
 
 class Animation extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleCollapsed = this.toggleCollapsed.bind(this);
+    this.state = {
+      cardCollapsed: false
+    }
+  }
+
+  toggleCollapsed(index) {
+    if(index === "parent") {
+      return this.setState({ cardCollapsed: !this.state.cardCollapsed});
+    }
+  }
+
   render() {
+    let applyCollapsedStyles = this.state.cardCollapsed ? 'collapsed' : '';
+
     return (
-      <div className="animation">
-        <Card index="1"/>
-        <Card index="2"/>
-        <Card index="3"/>
+      <div className={`animation ${applyCollapsedStyles}`}>
+        <Card index="1" toggleCollapsed={this.toggleCollapsed} cardType="parent"/>
+        <Card index="2" toggleCollapsed={this.toggleCollapsed} cardType="child"/>
+        <Card index="3" toggleCollapsed={this.toggleCollapsed} cardType="child"/>
       </div>
       )
   }
--- src/components/Animation Components/Card.js
+++ src/components/Animation Components/Card.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Icon from '../Icon';
+import Comment from '../../icons/comment.svg';
 
 class Card extends Component {
   constructor(props) {
     super(props);
     this.toggleOpen = this.toggleOpen.bind(this);
+    this.cardHandler = this.cardHandler.bind(this);
     this.state = {
       isOpen: false,
     }
@@ -14,7 +16,12 @@ class Card extends Component {
     this.setState({ isOpen : !this.state.isOpen });
   }
 
+  cardHandler() {
+    this.props.toggleCollapsed(this.props.cardType);
+  }
+
   render() {
+    let cardType = this.props.cardType
     let isOpen = this.state.isOpen ? 'expanded' : '';
 
     let bodyContent = () => {
@@ -29,7 +36,7 @@ class Card extends Component {
     let buttonGroup = () => {
       return (
         <div className="button-group">
-          <button className="button-item">
+          <button className="button-item" onClick={this.cardHandler}>
             <Icon object="contract_alt" type="utility" size="x-small" />
           </button>
           <button className="button-item">
@@ -38,8 +45,9 @@ class Card extends Component {
         </div>
       )
     }
+
     return (
-      <article className={`slds-card ${isOpen}`}>
+      <article className={`slds-card ${isOpen} ${cardType}`}>
         {buttonGroup()}
         <div className="slds-card__header">
           <header className="slds-media slds-media_center slds-has-flexi-truncate slds-m-bottom_xx-small">
@@ -48,7 +56,7 @@ class Card extends Component {
             </div>
             <div className="slds-media__body">
               <h2 className="slds-card__header-title">
-                  <span>Astro Trailblazer Trailblaze</span>
+                  <span>Astro Trailblazer</span>
               </h2>
             </div>
           </header>
@@ -56,7 +64,10 @@ class Card extends Component {
         </div>
         <div className="slds-card__body slds-card__body_inner">
           {isOpen ? bodyContent() : ''}
-          <button onClick={this.toggleOpen} className="show-more">{isOpen ? 'Show Less' : 'Show More'}</button>
+          <div className="card-footer">
+            <button onClick={this.toggleOpen} className="show-more">{isOpen ? 'Show Less' : 'Show More'}</button>
+            <img className="comment" src={Comment} alt="" />
+          </div>
         </div>
       </article>
       )
